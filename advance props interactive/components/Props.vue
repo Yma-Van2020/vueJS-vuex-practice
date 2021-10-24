68d2365858eb7b7a861589a9119ad7b546e8b2fb
Katelyn <74885386+Yma-Van2020@users.noreply.github.com>
2021-10-24 12:50:51 -0700

    Add files via upload

---
+++ advance props interactive/components/Props.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>My Show</h1>
+        
+        <p><strong>Title</strong> {{ title }}</p>
+        <p><strong>Episode #</strong> {{ episodeNumber }}</p>
+        <p><strong>Published</strong> {{ published }}</p>
+        <p v-show="isVisible"><strong>Optional</strong> {{ isVisible }}</p>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        props:{
+            title:{
+                type:String,
+                default: 'This is the default title'
+            },
+            published:{
+                type:[Boolean, Number],
+                default: false
+            },
+            episodeNumber:{
+                type:Number,
+                default: 18
+            },
+            isVisible:{
+                type:Boolean,
+                default:true
+            }
+        },
+        
+    }
+</script>
+
+<style>
+    
+</style>
